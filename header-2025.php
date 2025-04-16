278b9c91009928cfa0e5bcb61385f180b0d24d50
Russell Maier <russ@earthen.io>
2025-04-17 00:01:07 +0700

    Adding logos

--- header-2025.php
+++ header-2025.php
@@ -541,13 +541,13 @@ display: none;
 
                 <div id="language-code" onclick="showLangSelector()" aria-label="Switch languages">🌐 <span data-lang-id="000-language-code">EN</span></div>
 
-                <button type="button" class="top-search-button"  onclick="openSearch()"  aria-label="Search site">
+                <!-- <button type="button" class="top-search-button"  onclick="openSearch()"  aria-label="Search site">
                 </button>
 
-                <!--<button type="button" class="top-login-button" onclick="showLoginSelector()" aria-label="Login options"></button>
-
-                <button type="button" class="top-lang-button" onclick="showLangSelector()" aria-label="Switch languages"></button>
+                <button type="button" class="top-login-button" onclick="showLoginSelector()" aria-label="Login options"></button>
 -->
+                <button type="button" class="top-lang-button" onclick="showLangSelector()" aria-label="Switch languages"></button>
+
                 <dark-mode-toggle
                 id="dark-mode-toggle-5" style="min-width:82px;margin-top:-5px;margin-bottom:-15px;"
                 class="slider"
