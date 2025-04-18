0e4dccafe64f10e7fc422381e2196c2cdb088bcf
Russell Maier <russ@earthen.io>
2025-04-18 20:55:06 +0700

    Final

--- en/signup.php
+++ en/signup.php
@@ -358,45 +358,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-
-
-    function openTermsModal() {
-      closeSettings();
-
-      const modal = document.getElementById('form-modal-message');
-      const modalBox = document.getElementById('modal-content-box');
-
-      modal.style.display = 'flex';
-      modalBox.style.flexFlow = 'column';
-      document.getElementById('page-content')?.classList.add('blurred');
-      document.getElementById('footer-full')?.classList.add('blurred');
-      document.body.classList.add('modal-open');
-
-      modalBox.style.maxHeight = '80vh';
-      modalBox.style.overflowY = 'auto';
-
-      // 🚨 No escaping or nl2br, just insert raw HTML
-      modalBox.innerHTML = `<?= $app_info['app_terms_txt']; ?>`;
-    }
-
-
-    function openPrivacyModal() {
-      closeSettings();
-
-      const modal = document.getElementById('form-modal-message');
-      const modalBox = document.getElementById('modal-content-box');
-
-      modal.style.display = 'flex';
-      modalBox.style.flexFlow = 'column';
-      document.getElementById('page-content')?.classList.add('blurred');
-      document.getElementById('footer-full')?.classList.add('blurred');
-      document.body.classList.add('modal-open');
-
-      modalBox.style.maxHeight = '80vh';
-      modalBox.style.overflowY = 'auto';
-
-      modalBox.innerHTML = `<?= $app_info['app_privacy_txt']; ?>`;
-    }
+<?php require_once ("../scripts/app-modals.php");?>
 
 
 </script>
