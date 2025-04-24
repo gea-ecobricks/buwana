d5338ef7f4f907c5a3a27319dbd964f9c31afbeb
Russell Maier <russ@earthen.io>
2025-04-24 20:02:43 +0700

    Final

--- includes/signup-inc.php
+++ includes/signup-inc.php
@@ -398,6 +398,36 @@
 }
 
 
+/* Confirm email */
+
+
+  .code-boxes {
+        display: flex;
+        justify-content: center;
+        gap: 10px;
+    }
+    .code-box {
+        text-align: center;
+        font-family: 'Arvo', serif;
+        font-size: 2em;
+        max-width: 3em;
+    }
+    #second-code-confirm {
+        display: none;
+    }
+
+
+    .hidden {
+        display: none;
+    }
+    .error {
+        color: red;
+    }
+    .success {
+        color: green;
+    }
+
+
 </STYLE>
 
 
