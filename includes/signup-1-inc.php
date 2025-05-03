58ca68fdeb0cf6a98a5bdae60a43c8e478914a1f
Russell Maier <russ@earthen.io>
2025-05-03 19:11:08 +0700

    Fixing up signup-1 languages

--- includes/signup-1-inc.php
+++ includes/signup-1-inc.php
@@ -5,14 +5,14 @@
 <style>
     @media (prefers-color-scheme: light) {
         .app-signup-banner {
-            background: url('<?= htmlspecialchars($app_info['signup_top_img_url']) ?>') no-repeat center;
+            background: url('<?= htmlspecialchars($app_info['signup_top_img_url']) ?>?v=2') no-repeat center;
             background-size: contain;
         }
     }
 
     @media (prefers-color-scheme: dark) {
         .app-signup-banner {
-            background: url('<?= htmlspecialchars($app_info['signup_top_img_dark_url']) ?>') no-repeat center;
+            background: url('<?= htmlspecialchars($app_info['signup_top_img_dark_url']) ?>?v=2') no-repeat center;
             background-size: contain;
         }
     }
