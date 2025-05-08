e5524c53d1e5eb6bb7168165a952bf3d0d24dccf
Russell Maier <russ@earthen.io>
2025-05-08 11:02:44 +0700

    Launching modals on signup-4

---
+++ translations/signup-6-de.js
@@ -0,0 +1,46 @@
+/*-----------------------------------
+
+-----------------------------------*/
+// GERMAN TEXT SNIPPETS FOR SIGNUP-6
+/*-----------------------------------
+
+-----------------------------------*/
+
+const de_Page_Translations = {
+    "001-subs-set": "✔ Ihre terrestrischen Abonnements wurden bestätigt!",
+    "002-fun-part": "Jetzt kommt der spaßige Teil!",
+    "003-to-finalize": "Um Ihr Konto zu vervollständigen, wählen Sie ein Erdling-Emoji, das Sie am besten repräsentiert.",
+    "004-mammals": "Säugetiere",
+    "004-marine": "Meerestiere",
+    "004-reptiles-amphibians": "Reptilien & Amphibien",
+    "004-birds": "Vögel",
+    "004-plants": "Pflanzen",
+    "004-insects": "Insekten",
+    "004-human-link": "Menschliche Verbindung",
+    "005-emoji-hint": "Wählen Sie ein Emoji, das Sie auf ",
+    "006-community-connect": "Mit einem Buwana-Konto können Sie sich mit Ihrer lokalen Gemeinschaft verbinden...",
+    "007-community-placeholder": "Geben Sie Ihre Gemeinschaft ein...",
+    "008-start-typing-community": "Beginnen Sie mit der Eingabe, um eine Gemeinschaft zu sehen und auszuwählen. Es ist gut möglich, dass bereits jemand in Ihrer Nähe eine erstellt hat!",
+    "009-add-community": "Sie sehen Ihre Gemeinschaft nicht? Fügen Sie sie hinzu.",
+    "010-check-country": "Bitte stellen Sie sicher, dass wir Sie mit dem richtigen Land verbunden haben:",
+    "010b-select-country-option": "-- Wählen Sie Ihr Land --",
+    "011-confirm-language-choice": "Bitte stellen Sie sicher, dass Ihre Sprache korrekt erkannt wurde...",
+    "012-select-language-option": "-- Wählen Sie Ihre Sprache --",
+    "013-all-done-button": "Fertig! 👌",
+    "013b-ready-to-login": "Sie sind jetzt bereit, sich anzumelden!",
+
+    "014-add-community-title": "Fügen Sie Ihre Gemeinschaft hinzu",
+    "015-add-community-desc": "Fügen Sie Ihre Gemeinschaft zu Buwana hinzu, damit andere über regenerative Apps eine Verbindung herstellen können.",
+    "016-community-name-label": "Name der Gemeinschaft:",
+    "017-community-type-label": "Art der Gemeinschaft:",
+    "018-select-type-option": "Typ auswählen",
+    "019-type-neighborhood": "Nachbarschaft",
+    "020-type-city": "Stadt",
+    "021-type-school": "Schule",
+    "022-type-organization": "Organisation",
+    "023-country-label": "Land:",
+    "024-select-country-option": "Land auswählen...",
+    "025-language-label": "Bevorzugte Sprache:",
+    "026-select-language-option": "Sprache auswählen...",
+    "027-submit-button": "➕ Gemeinschaft erstellen",
+};
