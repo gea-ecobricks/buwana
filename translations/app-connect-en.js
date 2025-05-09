ed1bb510525303177729c6c364ae98739753bdb3
Russell Maier <russ@earthen.io>
2025-05-09 12:19:38 +0700

    Cleaning up the repo

---
+++ translations/app-connect-en.js
@@ -0,0 +1,18 @@
+/*-----------------------------------
+TEXT TRANSLATION SNIPPETS FOR GOBRIK.com
+-----------------------------------*/
+// ENGLISH SNIPPETS FOR SIGNUP-7
+
+
+const en_Page_Translations = {
+    "001-hurray": "Hurray",
+    "002-your-buwana-create": "Your Buwana account has been created.",
+    "003-you-will-be": "You'll be redirected to login to ",
+    "004-after": "after",
+    "005-seconds": "seconds",
+    "006-manual-redirect": 'If you\'re not redirected automatically,',
+    "007-click-here": "click here",
+
+};
+
+
