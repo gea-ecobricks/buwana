02576f3f9a68fd5fd25efb526e01937f0f63b621
Russell Maier <russ@earthen.io>
2025-05-13 08:59:29 +0700

    Cleaning up the repo

---
+++ meta/signup-5-id.php
@@ -0,0 +1,20 @@
+<title>Pilih langganan Terestrial Anda | Akun Buwana</title>
+
+<meta name="keywords" content="buwana, daftar, pendaftaran, mendaftar, gabung">
+<meta name="description" content="Pilih buletin email Terestrial yang menarik bagi Anda.">
+<meta name="author" content="Terestrial">
+<meta name="last-modified" content="' . $lastModified . '">
+<meta name="revised" content="' . $lastModified . '">
+
+<!-- Facebook Open Graph Tags for social sharing-->
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
+<meta property="og:url"           content="https://www.buwana.ecobricks.org/' . $lang . '/' . $page . '">
+<meta property="og:type"          content="app">
+<meta property="og:title"         content="Pilih langganan Terestrial Anda | Akun Buwana">
+<meta property="og:description"   content="Pilih buletin email Terestrial yang menarik bagi Anda." >
+<meta property="og:image"         content="https://www.gobrik.com/images/social-banner-1200px.png" >
+<meta property="fb:app_id"        content="1781710898523821" >
+<meta property="og:image:width"   content="1200" >
+<meta property="og:image:height"  content="1000" >
+<meta property="og:image:alt"     content="Sebuah jalan metaforis berkelok-kelok ke kejauhan dengan berbagai konstruksi ecoladrillo dan bumi di sisinya serta logo GoBrik melayang di atasnya">
+<meta property="og:locale"        content="id_ID">
