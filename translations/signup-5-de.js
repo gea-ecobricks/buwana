a4d6e99da02adb0b2b8b1ac234bac5dfaceeec3d
Russell Maier <russ@earthen.io>
2025-05-13 15:27:38 +0700

    Cleaning up the repo

---
+++ translations/signup-5-de.js
@@ -0,0 +1,16 @@
+/*-----------------------------------
+TEXT TRANSLATION SNIPPETS FOR signup-5.php
+-----------------------------------*/
+
+const de_Page_Translations = {
+    "001-select-subs": "Wähle deine Earthen-Abonnements",
+    "002-sub-subtitle": "📬 Wir teilen Neuigkeiten per E-Mail.",
+    "003-get-your": "Unser <a href=\"#\" onclick=\"openAboutEarthen()\" class=\"underline-link\">Earthen-Newsletter</a> wurde vor 10 Jahren erstmals vom Land des Igorot-Volkes verschickt. Er ist weiterhin kostenlos, aber jetzt kannst du aus verschiedenen Kanälen wählen...</span>",
+    "004-later-upgrade": "Später kannst du auf ein kostenpflichtiges Abonnement upgraden, um die Bewegung zu unterstützen.",
+    "005-nice": "Super! Du bist bereits abonniert für:",
+    "006-choose": "Wähle unten aus, welche Abonnements du hinzufügen oder entfernen möchtest:",
+    "007-not-subscribed": " ist noch für keinen Earthen-Newsletter angemeldet.",
+    "008-almost-done": "Die Aktivierung deines Buwana-Kontos ist fast abgeschlossen. Das Beste kommt zum Schluss...",
+    "009-finalize-button": "🐣 Abschließen ➡",
+    "010-terms": "Earthen-Newsletter werden gemäß unseren ",
+};
