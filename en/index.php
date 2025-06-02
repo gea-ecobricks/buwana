b90501ff9e6fa1796e46dac5406b091ca1fa3fbc
Russell Maier <russ@earthen.io>
2025-06-02 11:40:07 +0700

    Updating earthcal login process for snap apps

--- en/index.php
+++ en/index.php
@@ -39,13 +39,14 @@ if ($app_results && $app_results->num_rows > 0) {
 
   <div class="form-container">
       <div id="top-page-image"
-                  style="width:100%;height:350px;background:no-repeat center;background-size: contain;"
+                  style="width:100%;height:350px;background:no-repeat center;background-size: contain;margin-top:-70px,margin-bottom:20px;"
                   data-light-img="../webps/top-buwana-landing-banner.webp"
                   data-dark-img="../webps/top-buwana-landing-banner.webp">
              </div>
-<p  data-lang-id="001-about-buwana-description">
+<h3  data-lang-id="001-about-buwana-description" style="text-align:center;">
       Buwana is an open-source login system for regenerative web applications developed by the Global Ecobrick Alliance. The Buwana protocol provides the a user authentication alternative for apps that want to escape corporate logins for an ecoystem of resonant, green for-Earth enterprises.
-    </p>
+    </h3>
+    <p data-lang-id="002-just-starting" style="text-align:center;">The Buwana protocol has only just launched as of June 2025.  Here's the apps that are using it so far...</p>
     <div class="app-grid">
       <?php foreach ($apps as $app):
           $client_id = urlencode($app['client_id']);
