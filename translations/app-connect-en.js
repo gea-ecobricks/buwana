48875ca927a16e61f6cd4a5bcb1379e23490b8e6
Russell Maier <russ@earthen.io>
2025-06-03 12:04:23 +0700

    Fixing app texts

--- translations/app-connect-en.js
+++ translations/app-connect-en.js
@@ -5,13 +5,8 @@ TEXT TRANSLATION SNIPPETS FOR GOBRIK.com
 
 
 const en_Page_Translations = {
-    "001-hurray": "Hurray",
-    "002-your-buwana-create": "Your Buwana account has been created.",
-    "003-you-will-be": "You'll be redirected to login to ",
-    "004-after": "after",
-    "005-seconds": "seconds",
-    "006-manual-redirect": 'If you\'re not redirected automatically,',
-    "007-click-here": "click here",
+
+
 
 };
 
