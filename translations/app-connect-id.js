66dd82cd1574052c8e02648801679ce7c2faac3a
GEA Admin <64298789+gea-ecobricks@users.noreply.github.com>
2025-06-03 12:18:23 +0700

    Add translations for app-connect and bump version

--- translations/app-connect-id.js
+++ translations/app-connect-id.js
@@ -4,8 +4,15 @@ APP-CONNECT.PHP Translations
 
 
 const id_Page_Translations = {
-
-
-
-
+    "001-first-time-to-connect": "Hubungkan ke",
+    "001-authorized-app": "adalah aplikasi Buwana resmi",
+    "002-looks-like": "sepertinya Anda mencoba masuk ke",
+    "003-to-do-so": "Untuk melakukannya, kami akan menghubungkan akun Buwana Anda ke",
+    "004-buwana-profile": "Profil Buwana",
+    "005-data-essentials": "Data pengguna penting untuk masuk dan menggunakan aplikasi...",
+    "004-will-be-granted": "Hubungkan untuk memberi izin dan mulai di",
+    "009-connect-button": "Hubungkan",
+    "1000-terms-of-use": "Syarat Penggunaan",
+    "010-no-connect": "↩ Atau kembali ke",
+    "000-home": "beranda"
 };
--- translations/app-connect-zh.js
+++ translations/app-connect-zh.js
@@ -4,8 +4,15 @@ APP-CONNECT.PHP Translations
 
 
 const zh_Page_Translations = {
-
-
-
-
+    "001-first-time-to-connect": "连接到",
+    "001-authorized-app": "是授权的 Buwana 应用",
+    "002-looks-like": "看起来你正在尝试登录到",
+    "003-to-do-so": "为此，我们将把你的 Buwana 账户连接到",
+    "004-buwana-profile": "Buwana 个人资料",
+    "005-data-essentials": "用于登录和使用应用的基本用户数据...",
+    "004-will-be-granted": "连接以授权并开始使用",
+    "009-connect-button": "连接",
+    "1000-terms-of-use": "使用条款",
+    "010-no-connect": "↩ 或返回",
+    "000-home": "首页"
 };
