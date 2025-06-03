436968e85566c8fba822eefad4d93728c360024f
Russell Maier <russ@earthen.io>
2025-06-03 12:35:20 +0700

    Fixing app texts

--- meta/login-en.php
+++ meta/login-en.php
@@ -1,4 +1,4 @@
-<title>Login | GoBrik 3.0</title>
+<title>Login | Buwana App Manager</title>
 
 <meta name="keywords" content="login, gobrik, ecobrick app, signup, sign up, sign, up">
 <meta name="description" content="Login to the Gobrik app to manage your ecobrick, plastic and projects.">
@@ -10,12 +10,12 @@
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" >
 <meta property="og:url"           content="https://www.gobrik.com/' . $lang . '/' . $page . '">
 <meta property="og:type"          content="app">
-<meta property="og:title"         content="Login | GoBrik">
-<meta property="og:description"   content="Login to the Gobrik app to manage your ecobrick, plastic and projects." >
-<meta property="og:image"         content="https://www.gobrik.com/images/social-banner-1200px.png" >
+<meta property="og:title"         content="Login | Buwana App Manager">
+<meta property="og:description"   content="Login to launch or manage your app in the Buwana ecosystem." >
+<meta property="og:image"         content="https://buwana.ecobricks.org/pngs/buwana-banner-2025.png" >
 <meta property="fb:app_id"        content="1781710898523821" >
-<meta property="og:image:width"   content="1200" >
-<meta property="og:image:height"  content="1000" >
-<meta property="og:image:alt"     content="A metaphorical road winding into the distance with various ecobrick and earth constructions along side it and the GoBrik logo floating above">
+<meta property="og:image:width"   content="1000" >
+<meta property="og:image:height"  content="599" >
+<meta property="og:image:alt"     content="A vision of our transition from modern to diverse, vibrant and abundant Earthen integration.">
 <meta property="og:locale"        content="en_GB">
 
