390ed062ac7ff8c2dec6c0d2c160778ece6ba2e4
Russell Maier <russ@earthen.io>
2025-06-15 14:14:49 +0700

    Fixing app texts

--- translations/login-en.js
+++ translations/login-en.js
@@ -4,20 +4,7 @@ ENGLISH LOGIN TRANSLATION
 
 
 const en_Page_Translations = {
-    "001-cant-find": "🤔 We can't find this credential in the database.",
-    "002-password-is-wrong": "👉 Password is wrong.",
-    "003-forgot-your-password": "Forgot your password?",
-    "000-reset-it": "Reset it.",
-    "003-code-status": "A code to login will be sent to your email.",
-    "004-login-button": '<input type="submit" id="submit-password-button" value="Login" class="login-button-75">',
-    "005-password-field-placeholder": '<input type="password" id="password" name="password" required placeholder="Your password...">',
-};
 
-/*-----------------------------------
-LOGIN STATUS MESSAGES
-----------------------------------*/
-
-const en_LoginStatusMessages = {
     logout: {
         main: "You're logged out.",
         sub: "When you're ready $first_name, login again with your account credentials."
@@ -33,7 +20,20 @@ const en_LoginStatusMessages = {
     default: {
         main: "Welcome back!",
         sub: "Please login again with your account credentials."
-    }
+    },
+
+
+    "001-cant-find": "🤔 We can't find this credential in the database.",
+    "002-password-is-wrong": "👉 Password is wrong.",
+    "003-forgot-your-password": "Forgot your password?",
+    "000-reset-it": "Reset it.",
+    "003-code-status": "A code to login will be sent to your email.",
+    "004-login-button": '<input type="submit" id="submit-password-button" value="Login" class="login-button-75">',
+    "005-password-field-placeholder": '<input type="password" id="password" name="password" required placeholder="Your password...">',
+
+
+
+
 };
 
 
