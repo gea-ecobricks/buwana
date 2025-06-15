aef00cb9c31e2bf4a96ac95bf701a0d1adbcb2bf
Russell Maier <russ@earthen.io>
2025-06-15 14:22:24 +0700

    Fixing app texts

--- en/login.php
+++ en/login.php
@@ -19,7 +19,7 @@ if (!empty($app_info['client_id'])) {
 
 // Set page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.774';
+$version = '0.775';
 $page = 'login';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 $is_logged_in = false; // Ensure not logged in for this page
