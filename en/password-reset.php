6ce422e0555cc8a4038b6c7db818282a7650bfce
Russell Maier <russ@earthen.io>
2025-06-22 14:54:00 +0700

    Fixing app texts

--- en/password-reset.php
+++ en/password-reset.php
@@ -3,7 +3,7 @@ require_once '../earthenAuth_helper.php'; // Include the authentication helper f
 
 // Set page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.778';
+$version = '0.779';
 $page = 'reset';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 
