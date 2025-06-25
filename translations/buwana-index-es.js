d82fd630cfd01ac7fe67e093f78553b4ef41c4f8
Russell Maier <68637813+russs95@users.noreply.github.com>
2025-06-25 10:27:09 +0700

    Add translation files for Buwana index

---
+++ translations/buwana-index-es.js
@@ -0,0 +1,15 @@
+/*------------------------------------
+APP-CONNECT.PHP Translations
+ */
+
+const es_Page_Translations = {
+
+        "001-about-buwana": "Buwana es un sistema de inicio de sesión de código abierto para aplicaciones web regenerativas desarrollado por la Alianza Global de Ecoladrillos.",
+        "002-buwana-desc": "El protocolo Buwana brinda una alternativa de autenticación de usuarios para las apps que quieren escapar de los inicios de sesión corporativos y apoyar un ecosistema de empresas resonantes y verdes para la Tierra. El protocolo Buwana acaba de lanzarse en junio de 2025. Estas son las aplicaciones que lo están usando hasta ahora...",
+        "000-login": "Iniciar sesión",
+        "000-signup": "Registrarse",
+        "000-about": "ℹ️ Acerca de",
+        "003-open-source": "El código y la wiki de documentación de Buwana están en Github",
+        "004-view-repo": "Ver repositorio ↗",
+    };
+
