f4791bf253560e8d4092a1e60e334764265a8f92
Russell Maier <russ@earthen.io>
2025-06-25 09:25:37 +0700

    Fixing app texts

--- meta/buwana-index-en.php
+++ meta/buwana-index-en.php
@@ -1,4 +1,4 @@
-<title>Buwana SSO</title>
+<title>Buwana App Ecosystem | Green SSO</title>
 
 <meta name="keywords" content="buwana, earthAuth, Auth0, authentication, open source, regenerative apps, SSO, single singn on,">
 <meta name="description" content="Register for a Buwana account to use on <?= htmlspecialchars($app_info['app_display_name']) ?>">
@@ -8,13 +8,13 @@
 
 <!-- Facebook Open Graph Tags for social sharing-->
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" >
-<meta property="og:url"           content="https://www.buwana.ecobricks.org/' . $lang . '/">
+<meta property="og:url"           content="https://www.buwana.ecobricks.org/en/">
 <meta property="og:type"          content="app">
-<meta property="og:title"         content="Buwana SSO | Home">
-<meta property="og:description"   content="Buwana is an open source SSO service for regenerative apps." >
+<meta property="og:title"         content="Buwana App Ecosystem | Green SSO">
+<meta property="og:description"   content="Buwana is an open source SSO service for an ecosystem regenerative apps." >
 <meta property="og:image"         content="https://buwana.ecobricks.org/pngs/buwana-banner-2025.png" >
 <meta property="fb:app_id"        content="1781710898523821" >
 <meta property="og:image:width"   content="1000" >
 <meta property="og:image:height"  content="600" >
-<meta property="og:image:alt"     content="A river winding through a common home nightscape.">
+<meta property="og:image:alt"     content="A river winding through a common home nightscape with the moon and stars.">
 <meta property="og:locale"        content="en_GB">
